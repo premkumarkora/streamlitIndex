cc9529e79bc37f31b5816d568800f86ece7362df
PremKumar Kora <49707137+premkumarkora@users.noreply.github.com>
2022-10-18 10:29:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ icon_size = 20
 
 st_button('medium', 'https://consultkora.medium.com/', 'Read my Blogs', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/premkumarkora/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
+st_button('newsletter', 'https://sendfox.com/consultkora', 'Sign up for my Newsletter', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/consultkora', 'Buy me a Coffee', icon_size)
